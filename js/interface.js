470a3dd678f097493b1e937a46bb3fafa3ba609f
azmansalleh <azman_master@hotmail.com>
2018-10-27 19:45:40 +0800

    Modified frontend with dependencies added

--- js/interface.js
+++ js/interface.js
@@ -1,17 +1,51 @@
 //Map buttons and interfaces
-var sideModal = document.getElementById("houseModal");
-var houseButton = document.getElementById("houseButton");
-
-var closeModal = document.getElementsByClassName("closeModal");
-
 var zoomIn = document.getElementById("zoomIn");
 var zoomOut = document.getElementById("zoomOut");
 
+var closeModal = document.getElementsByClassName("closeModal");
 
-houseButton.onclick = function(){
-	sideModal.classList.toggle("clicked");
+var propertyModal = document.getElementById("propertyModal");
+var propertyButton = document.getElementById("propertyButton");
+
+var amenitiesModal = document.getElementById("amenitiesModal");
+var amenitiesButton = document.getElementById("amenitiesButton");
+
+var grantModal = document.getElementById("grantModal");
+var grantButton = document.getElementById("grantButton")
+
+var profileModal = document.getElementById("profileModal");
+var profileButton = document.getElementById("profileButton")
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+var y = document.getElementById("f");
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    y.innerHTML = this.value + "km";
+}
+
+
+propertyButton.onclick = function(){
+	propertyModal.classList.toggle("clicked");
 	console.log("House show");
 };
+
+amenitiesButton.onclick = function(){
+	amenitiesModal.classList.toggle("clicked");
+	console.log("Amenities show");
+};
+
+grantButton.onclick = function(){
+	grantModal.classList.toggle("clicked");
+	console.log("Amenities show");
+};
+
+profileButton.onclick = function(){
+	profileModal.classList.toggle("clicked");
+	console.log("Profile show");
+};
+
 
 closeModal.onclick = function(){
 	console.log("Close Modal");
